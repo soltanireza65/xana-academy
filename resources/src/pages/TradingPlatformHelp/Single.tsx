c78072376bb87a3bb1ad87fbd66470efd87fd637
Reza Soltani <soltanireza65@gmail.com>
2023-03-07 19:25:12 +0000

    ...

--- resources/src/pages/TradingPlatformHelp/Single.tsx
+++ resources/src/pages/TradingPlatformHelp/Single.tsx
@@ -3,11 +3,11 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { createRoot } from "react-dom/client";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
-import Content from "../../components/help/Content";
 import Sidebar from "../../components/help/Sidebar";
+import Content from "../../components/help/Content";
 const container = document.getElementById("single_help");
 const root = createRoot(container!);
-
+// 
 const Single = () => {
   return (
     <SimpleGrid columns={{ base: 1, md: 2, lg: 3 }} spacing={10}>
